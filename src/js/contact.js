f0562bfec7a3d02adbb607f989e77db3db9ed47e
karpik <p.karpik11072001@gmail.com>
2024-05-26 09:33:49 +0300

    Created contact:get-started section

--- src/js/contact.js
+++ src/js/contact.js
@@ -5,9 +5,11 @@ import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
 import { useAccordion } from './components/accordion.js';
 import { usePartnerSlider } from './components/partners.js';
+import { usePhone } from './components/contact/phone.js';
 
 
 useTheme();
 useBurger();
 useAccordion();
 usePartnerSlider();
+usePhone();
