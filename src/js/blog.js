d2afb51e3c44cec3a82781750aa4908a0a4220ff
karpik <p.karpik11072001@gmail.com>
2024-05-28 13:04:02 +0300

    Created blog

---
+++ src/js/blog.js
@@ -0,0 +1,11 @@
+import '/scss/blog.scss';
+
+// components
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { useAccordion } from './components/accordion.js';
+
+
+useTheme();
+useBurger();
+useAccordion();
